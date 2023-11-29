d5d6db6902f9024468fd83f1225c1e35ae4d0bc1
Elias Xu <flam0799@gmail.com>
2023-11-29 06:04:01 -0500

    setup

---
+++ src/monaco-yaml-customtag.js
@@ -0,0 +1,44 @@
+// yaml-extension.js
+
+// Check if Monaco Editor is defined
+if (typeof monaco !== 'undefined') {
+
+    console.log(monaco);
+    // Register YAML language
+    monaco.languages.register({ id: 'yaml' });
+  
+    // Register autocompletion provider
+    monaco.languages.registerCompletionItemProvider('yaml', {
+      provideCompletionItems: () => {
+        return [
+          { label: '!customTag1', kind: monaco.languages.CompletionItemKind.Keyword },
+          { label: '!customTag2', kind: monaco.languages.CompletionItemKind.Keyword },
+        ];
+      },
+    });
+  
+    // Register hover provider
+    monaco.languages.registerHoverProvider('yaml', {
+      provideHover: (model, position) => {
+        const wordAtPosition = model.getWordAtPosition(position);
+        if (wordAtPosition) {
+          const word = wordAtPosition.word;
+          if (word === '!customTag1') {
+            return {
+              contents: [{ value: 'Description for Custom Tag 1' }],
+              range: wordAtPosition.range,
+            };
+          } else if (word === '!customTag2') {
+            return {
+              contents: [{ value: 'Description for Custom Tag 2' }],
+              range: wordAtPosition.range,
+            };
+          }
+        }
+        return null;
+      },
+    });
+  } else {
+    console.error('Monaco Editor is not defined. Make sure it is loaded before this script.');
+  }
+  
